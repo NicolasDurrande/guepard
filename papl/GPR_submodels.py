1f23ccccf8069137142c444b5dd1bfb68fe5fe2d
nicolas@secondmind.ai <nicolas@nicolas-lptp.piointernal.prowler.io>
2022-04-07 10:10:55 +0100

    first test running

--- papl/GPR_submodels.py
+++ papl/GPR_submodels.py
@@ -1,19 +1,21 @@
+from typing import List, Optional
+
 import gpflow
-
-
 from gpflow.base import RegressionData
 from gpflow.kernels import Kernel
 from gpflow.mean_functions import MeanFunction
-from typing import List, Optional
 
 
-def get_gpr_submodels(data_list: List[RegressionData],
-                      kernel: Kernel,
-                      mean_function: Optional[MeanFunction] = None,
-                      noise_variance: float = .1):
+def get_gpr_submodels(
+    data_list: List[RegressionData],
+    kernel: Kernel,
+    mean_function: Optional[MeanFunction] = None,
+    noise_variance: float = 0.1,
+):
     """
     Helper function to build a list of GPflow GPR submodels from a list of datasets and a GP prior, and a likelihood variance
     """
-    return [gpflow.models.GPR(data, kernel, mean_function, noise_variance) for data in data_list]
-
-
+    return [
+        gpflow.models.GPR(data, kernel, mean_function, noise_variance)
+        for data in data_list
+    ]
--- tests/test_papl.py
+++ tests/test_papl.py
@@ -1,5 +1,18 @@
 from papl import __version__
+import numpy as np
+from papl.GPR_submodels import get_gpr_submodels
 
-
+import gpflow
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
+def test_get_gpr_submodels():
+    data = ((np.random.uniform(size=(10, 2)), np.random.normal(size=(10, 1))) for _ in range(3))
+    kernel = gpflow.kernels.Matern32()
+    M = get_gpr_submodels(data, kernel)
+
+    assert len(M)==3, "The length of the model list isn't equal to the length of\
+        the data list"
+    
+    # smoke test on model prediction
+    M[1].predict_f(np.random.uniform(np.random.uniform(size=(3, 2))))
