b0c0a21b623bc1e31d081c2964d1cd6bd50fc9ff
nicolas@secondmind.ai <nicolas@nicolas-lptp.piointernal.prowler.io>
2022-04-08 16:28:51 +0100

    adding papl model aggregation based on full cov

--- papl/papl.py
+++ papl/papl.py
@@ -1,15 +1,23 @@
 from typing import List
 
 import gpflow
+import numpy as np
 import tensorflow as tf
+from gpflow.base import InputData, MeanAndVariance
 from gpflow.models import GPModel
 
 jitter = gpflow.config.default_jitter()
 
 
 class PAPL(GPModel):
-    def __init__(self, models: List[GPModel]):
+    """
+    Posterior Aggregation with Pseudo-Likelihood: Base class for merging submodels using the pseudo-likelihood method.
+    """
 
+    def __init__(self, models: List[GPModel]):
+        """
+        :param models: A list of GPflow models with the same prior and likelihood.
+        """
         # check that all models have the same prior
         for model in models[1:]:
             assert (
@@ -34,18 +42,25 @@ class PAPL(GPModel):
         )
         self.models = models
 
-    def _predict_f_marginals(self, x):
+    def predict_f_marginals(self, Xnew: InputData) -> MeanAndVariance:
+        """
+        Fastest method for aggregating marginal submodel predictions.
+        For more accurate predictions (but with higher computational cost
+        and possibly lower numerical stability), see `predict_f` method.
+        :param Xnew: 2D Array or tensor corresponding to points in the input
+        where we want to make prediction.
+        """
         # prior predictions
-        mp = self.mean_function(x)[:, :, None]  # shape is [n, 1, 1]
-        vp = self.kernel.K_diag(x)[:, None, None]  # [n, 1, 1]
+        mp = self.mean_function(Xnew)[:, :, None]  # shape is [n, 1, 1]
+        vp = self.kernel.K_diag(Xnew)[:, None, None]  # [n, 1, 1]
 
         # submodel predictons
-        preds = [m.predict_f(x) for m in self.models]
+        preds = [m.predict_f(Xnew) for m in self.models]
         Me = tf.stack([pred[0] for pred in preds], axis=2)  # [n, latent, sub]
         Ve = tf.stack([pred[1] for pred in preds], axis=2)
 
         # equivalent pseudo observations that would turn
-        # the prior at x into the expert posterior at x
+        # the prior at Xnew into the expert posterior at Xnew
         pseudo_noise = vp * Ve / (vp - Ve + jitter)
         pseudo_y = mp + vp / (vp - Ve + jitter) * (Me - mp)
 
@@ -57,8 +72,53 @@ class PAPL(GPModel):
 
         return mean, var
 
-    def maximum_log_likelihood_objective(self):
-        pass
+    def predict_f(self, Xnew: InputData, full_cov: bool = False) -> MeanAndVariance:
+        """
+        Prediction method based on the aggregation of multivariate submodel predictions.
+        For more faster predictions and possibly more numerically stable predictions
+        (but with lower accuracy), see the `predict_f_marginals` method.
+        :param Xnew: 2D Array or tensor corresponding to points in the input
+        where we want to make prediction.
+        :param full_cov: Wether or not to return the full posterior covariance matrix.
+        """
+        len(self.models)
 
-    def predict_f(self, Xnew):
+        # prior distribution
+        mp = self.mean_function(Xnew)[None, :, :]  # [1, N, L]
+        vp = self.kernel.K(Xnew)[None, None, :, :]  # [1, L, N, N]
+
+        # expert distributions
+        preds = [m.predict_f(Xnew, full_cov=True) for m in self.models]
+        Me = tf.concat([pred[0][None, :, :] for pred in preds], axis=0)  # [P, N, L]
+        Ve = tf.concat(
+            [pred[1][None, :, :, :] for pred in preds], axis=0
+        )  # [P, L, N, N]
+
+        # equivalent pseudo observations that would turn
+        # the prior at Xnew into the expert posterior at Xnew
+        Jitter = jitter * np.eye(Xnew.shape[0])[None, None, :, :]
+        pseudo_noise = vp @ tf.linalg.inv(vp - Ve + Jitter) @ vp - vp
+        # pseudo_noise = vp @ tf.linalg.inv(vp - Ve ) @ Ve
+        # pseudo_noise = tf.linalg.inv(tf.linalg.inv(vp) - tf.linalg.inv(Ve))
+        pseudo_y = (
+            mp
+            + vp
+            @ tf.linalg.inv(vp - Ve + Jitter)
+            @ tf.transpose(Me - mp, (0, 2, 1))[:, :, :, None]
+        )  # [P, L, N, 1]
+        # pseudo_y = mp + pseudo_noise @ tf.linalg.inv(Ve) @ (Me - mp)
+
+        # print(np.max(np.abs(pseudo_noise - pseudo_noise_old)))
+        # prediction
+        var = tf.linalg.inv(
+            tf.linalg.inv(vp[0, :, :])
+            + tf.reduce_sum(tf.linalg.inv(pseudo_noise), axis=0)
+        )
+        mean = var @ (
+            tf.linalg.inv(vp[0, :, :]) @ mp[0, :, :]
+            + tf.reduce_sum(tf.linalg.inv(pseudo_noise) @ pseudo_y, axis=0)
+        )
+        return tf.transpose(mean[:, :, 0]), var
+
+    def maximum_log_likelihood_objective(self):
         pass
--- tests/test_papl.py
+++ tests/test_papl.py
@@ -26,7 +26,7 @@ def test_get_gpr_submodels():
 
 
 @pytest.mark.parametrize("num_latent", [1, 2])
-def test_papl_predict_marginals(num_latent):
+def test_papl_predict_f_marginals(num_latent):
     dim = 3
     num_data = 200
     num_split = 3
@@ -48,7 +48,7 @@ def test_papl_predict_marginals(num_latent):
     m_gpr = gpflow.models.GPR((X, Y), kernel, noise_variance=0.1)
 
     # Check "good" match between aggregated model and gpr at training points
-    mean_agg, var_agg = m_agg._predict_f_marginals(X)
+    mean_agg, var_agg = m_agg.predict_f_marginals(X)
     mean_gpr, var_gpr = m_gpr.predict_f(X)
 
     np.testing.assert_array_almost_equal(
@@ -65,7 +65,7 @@ def test_papl_predict_marginals(num_latent):
     )
 
     # Check "good" match between aggregated model and prior far away from training data
-    mean_agg, var_agg = m_agg._predict_f_marginals(X + 10.0)
+    mean_agg, var_agg = m_agg.predict_f_marginals(X + 10.0)
 
     np.testing.assert_array_almost_equal(
         mean_agg,
@@ -77,5 +77,61 @@ def test_papl_predict_marginals(num_latent):
         var_agg,
         np.ones_like(var_agg),
         decimal=4,
+        err_msg="PALP variance does not revert to prior far away from training data",
+    )
+
+
+@pytest.mark.parametrize("num_latent", [1, 2])
+def test_papl_predict_f(num_latent):
+    dim = 3
+    num_data = 200
+    num_split = 3
+    X = np.random.uniform(size=(num_data, dim))
+    Y = np.cos(X[:, :num_latent]) + np.random.normal(
+        0, np.sqrt(0.1), size=(num_data, 1)
+    )
+
+    Xl = np.array_split(X, num_split)
+    Yl = np.array_split(Y, num_split)
+
+    kernel = gpflow.kernels.Matern32()
+
+    # make submodels and aggregate them
+    M = get_gpr_submodels(zip(Xl, Yl), kernel)
+    m_agg = papl.PAPL(M)
+
+    # make a GPR model as baseline
+    m_gpr = gpflow.models.GPR((X, Y), kernel, noise_variance=0.1)
+
+    # Check "good" match between aggregated model and gpr at training points
+    mean_agg, var_agg = m_agg.predict_f(X, full_cov=True)
+    mean_gpr, var_gpr = m_gpr.predict_f(X, full_cov=True)
+
+    np.testing.assert_array_almost_equal(
+        mean_agg,
+        mean_gpr,
+        decimal=4,
+        err_msg="mismatch between the PALP and GPR mean predictions",
+    )
+    np.testing.assert_array_almost_equal(
+        var_agg,
+        var_gpr,
+        decimal=4,
+        err_msg="mismatch between the PALP and GPR var predictions",
+    )
+
+    # Check "good" match between aggregated model and prior far away from training data
+    mean_agg, var_agg = m_agg.predict_f(X + 10.0)
+
+    np.testing.assert_array_almost_equal(
+        mean_agg,
+        np.zeros_like(mean_agg),
+        decimal=5,
+        err_msg="PALP mean does not revert to prior far away from training data",
+    )
+    np.testing.assert_array_almost_equal(
+        var_agg[0, :, :],
+        m_gpr.kernel.K(X + 10.0),
+        decimal=5,
         err_msg="PALP variance does not revert to prior far away from training data",
     )
