e9037f9ae003ffc3e34a5879dc827043ac5f28ef
Vincent Dutordoir <dutordoirv@gmail.com>
2022-08-25 13:40:54 +0100

    size vs acc

--- notebooks/accuracy_vs_testsize.py
+++ notebooks/accuracy_vs_testsize.py
@@ -1,18 +1,15 @@
 # %%
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.stats import qmc
+import matplotlib.cm as cm
 
 import gpflow
 
 import guepard
 from guepard.utilities import get_gpr_submodels
 
-# %%
-NOISE_VAR = 1e-5
-NUM_DATAPOINTS_PER_DATASET = 16
 
-# %%
+#%%
 def plot_mean_conf(x, mean, var, ax, color='C0'):
     ax.plot(x, mean, color, lw=2)
     ax.fill_between(
@@ -23,6 +20,7 @@ def plot_mean_conf(x, mean, var, ax, color='C0'):
         alpha=0.2,
     )
 
+
 def plot_model(m, ax, x=np.linspace(0, 1, 101)[:, None], plot_data=True, color='C0'):
     if plot_data:
         X, Y = m.data
@@ -30,48 +28,43 @@ def plot_model(m, ax, x=np.linspace(0, 1, 101)[:, None], plot_data=True, color='
     
     mean, var = m.predict_f(x)
     plot_mean_conf(x, mean, var, ax, color)
-# %%
-
-
-
 
 # %%
-X = np.linspace(0, 1, 2**5)[:, None]
+NOISE_VAR = 1e-1
+NUM_SPLIT = 3
+LN_NUM_DATA = 5  # num_datapoints = 2 ** LN_NUM_DATA + 1
+# %%
+
+X = np.linspace(0, 1, 2**LN_NUM_DATA + 1)[:, None]
+print("num data", len(X))
 kernel = gpflow.kernels.SquaredExponential(lengthscales=.1)
 Y = gpflow.models.GPR((np.c_[-10.], np.c_[0.]), kernel, noise_variance=NOISE_VAR).predict_f_samples(X)
-
 full_gpr = gpflow.models.GPR((X, Y), kernel, noise_variance=NOISE_VAR)
-
+# %%
 fig, ax = plt.subplots()
 ax.plot(X, Y, "kx")
 plot_model(full_gpr, ax=ax, plot_data=False)
+
 #%%
-def get_subsets_data(data, num_datapoints_per_dataset):
-    X, Y = data
-    assert len(X) % num_datapoints_per_dataset == 0
-    datasets = []
-    num_datasets = len(X) // num_datapoints_per_dataset
-    for i in range(num_datasets):
-        X_ = X[i * num_datapoints_per_dataset: (i+1) * num_datapoints_per_dataset]
-        Y_ = Y[i * num_datapoints_per_dataset: (i+1) * num_datapoints_per_dataset]
-        datasets.append((X_, Y_))
-    return datasets
-
-
-datasets = get_subsets_data((X, Y), NUM_DATAPOINTS_PER_DATASET)
-print(len(datasets))
+x_list = np.array_split(X, NUM_SPLIT)  # list of num_split np.array
+y_list = np.array_split(Y, NUM_SPLIT)  
+datasets = list(zip(x_list, y_list))
 
 for i, data in enumerate(datasets):
     X_, Y_ = data
+    print(len(X_), len(Y_))
     plt.plot(X_, Y_, f"C{i%7}x")
 
-# %%
 
+# %%
 # mean_function = gpflow.mean_functions.Constant(0.5)
 submodels = get_gpr_submodels(datasets, kernel, mean_function=None, noise_variance=NOISE_VAR) # list of num_split GPR models
 
 # M is a list of GPR models, let's plot them
 fig, axes = plt.subplots(1, len(datasets), figsize=(16, 4))
+if len(datasets) == 1:
+    axes = [axes]
+
 x = np.linspace(-.5, 1.5, 101)[:, None]
 [plot_model(m, ax, x) for ax, m in zip(axes, submodels)];
 [ax.plot(X, Y, 'kx', mew=1., alpha=.1) for ax, _ in zip(axes, submodels)];
@@ -79,47 +72,53 @@ x = np.linspace(-.5, 1.5, 101)[:, None]
 # %%
 m_agg = guepard.EquivalentObsEnsemble(submodels)
 
-
-
-
 fig, ax = plt.subplots(1, 1, figsize=(6, 4))
-plot_model(m_agg, ax, plot_data=False)
-plot_model(full_gpr, ax, plot_data=False)
-plt.plot(X, Y, "kx")
+plot_model(m_agg, ax, plot_data=False, color="C0")
+plot_model(full_gpr, ax, plot_data=False, color="C1")
+ax.plot(X, Y, "kx")
 
 # %%
 
-def get_x_test(step: int) -> np.ndarray:
+def get_subset_of_data(A: np.ndarray, step: int) -> np.ndarray:
     """
-    returns:
-
-    step 1: [1/2],
-    step 2: [1/4, 2/4, 3/4],
-    step 3: [1/8, 2/8, ..., 7/8],
-    ...
-
-    importantly, the middle element always equals 0.5.
+    Returns a subarray of `A`. The elements in the subarray depend on `step`.
+    For `step` equal to 0, the subarray consists of a single element which will be
+    the the middle element of `A`. For `step` >= 1, the subarray contains the (2**step + 1)
+    middle elements.
     """
-    assert step > 0
-    xx = np.arange(1, 2**step) / 2**step
-    # assert xx[len(xx) // 2] == 0.5
-    return xx
+    middle_index = len(A)//2
+    if step == 0:
+        return A[[middle_index]]
+    else:
+        pad = int(2 ** (step - 1))  # 1, 2, 4, 8, ...
+        return A[middle_index - pad: middle_index + pad + 1]
+
+
+for step in range(LN_NUM_DATA + 1):
+    X_subset = get_subset_of_data(X, step)
+    assert len(X_subset) == 1 if step == 0 else (2 ** step + 1)
+    
+# %%
 
 
 mus = []
 sigmas = []
+len_x_subset = []
 
-for i in range(1, 12):
+for i in range(LN_NUM_DATA + 1):
     print("=" * 30, i)
-    xx = get_x_test(i)
-    m, v = m_agg.predict_f(xx[:, None])
+    xx = get_subset_of_data(X, i)
+    m, v = m_agg.predict_f(xx)
     mus.append(m.numpy().flatten()[len(xx) // 2])
     sigmas.append(v.numpy().flatten()[len(xx) // 2] ** 0.5)
+    len_x_subset.append(len(xx))
+    print(len_x_subset[-1], mus[-1], sigmas[-1])
 # %%
 
-m, v = full_gpr.predict_f(get_x_test(1)[:, None])
+m, v = full_gpr.predict_f(get_subset_of_data(X, 0))
 m_full = m.numpy().flatten()[0]
 sigma_full = v.numpy().flatten()[0] ** 0.5
+print(m_full, sigma_full)
 # %%
 
 def kl_univariate_gaussians(mu_1, sigma_1, mu_2, sigma_2):
@@ -136,10 +135,10 @@ kls = [
 
 colors = cm.viridis(np.linspace(0, 1, len(kls)))
 
-plt.scatter(np.arange(len(kls)), kls, color=colors)
+plt.scatter(len_x_subset, kls, color=colors)
 plt.ylabel("KL divergence")
-plt.xlabel("Size X*")
-plt.xticks(range(11), [fr"$2^{{{step}}}$" for step in range(11)])
+plt.xlabel("size X*")
+# plt.xticks(range(11), [fr"$2^{{{step}}}$" for step in range(11)])
 plt.yscale("log")
 plt.savefig('acc_vs_test_size__kl_div.png', facecolor="white", transparent=False)
 
@@ -147,7 +146,6 @@ plt.savefig('acc_vs_test_size__kl_div.png', facecolor="white", transparent=False
 theta_1 = np.array(mus) / np.array(sigmas)**2.
 theta_2 = 1. / (2 * np.array(sigmas)**2.)
 
-import matplotlib.cm as cm
 
 colors = cm.viridis(np.linspace(0, 1, len(theta_1)))
 
